50d03ecb8f355bed9d30c3f34bf28c33a4bc72b6
Nathan Phillips <Nathan.Phillips@clinisys.com>
2025-08-21 17:29:20 -0400

    Removed code returning all jobs as the list of enqueued jobs if no enqueued job ids were found. This was causing performance issues and is not consistent with Hangfire's original implementation.
    Added InstallIndexes.sql and added logic to OracleObjectInstaller to conditionally install them.

--- Hangfire.Oracle/OracleObjectsInstaller.cs
+++ Hangfire.Oracle/OracleObjectsInstaller.cs
@@ -16,21 +16,36 @@ namespace TH.Hangfire.Oracle
         public static void Install(IDbConnection connection, string schemaName)
         {
             if (connection == null) throw new ArgumentNullException(nameof(connection));
-
-            if (TablesExists(connection, schemaName))
+            var tablesExist = TablesExists(connection, schemaName);
+            var indexExists = IndexExists(connection, schemaName);
+            if (tablesExist && indexExists)
             {
-                Log.Info("DB tables already exist. Exit install");
+                Log.Info("DB tables and indexes already exist. Exit install");
                 return;
             }
+            if (!tablesExist)
+            {
+                Log.Info("Start installing Hangfire SQL objects...");
 
-            Log.Info("Start installing Hangfire SQL objects...");
+                var script = GetStringResource("Hangfire.Oracle.Install.sql");
 
-            var script = GetStringResource("Hangfire.Oracle.Install.sql");
+                var sqlCommands = script.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                sqlCommands.ToList().ForEach(s => connection.Execute(s));
 
-            var sqlCommands = script.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-            sqlCommands.ToList().ForEach(s => connection.Execute(s));
+                Log.Info("Hangfire SQL objects installed.");
+            }
 
-    Log.Info("Hangfire SQL objects installed.");
+            if (!indexExists)
+            {
+                Log.Info("Start installing Hangfire Indexes...");
+
+                var script = GetStringResource("Hangfire.Oracle.InstallIndexes.sql");
+
+                var sqlCommands = script.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                sqlCommands.ToList().ForEach(s => connection.Execute(s));
+
+                Log.Info("Hangfire Indexes installed.");
+            }
         }
 
         private static bool TablesExists(IDbConnection connection, string schemaName)
@@ -39,20 +54,30 @@ namespace TH.Hangfire.Oracle
 
             if (!string.IsNullOrEmpty(schemaName))
             {
-                tableExistsQuery = $@"SELECT TABLE_NAME
-FROM all_tables
-WHERE OWNER = '{schemaName}' AND TABLE_NAME LIKE 'HF_%'
-ORDER BY TABLE_NAME";
+                tableExistsQuery = $@"SELECT TABLE_NAME FROM all_tables WHERE OWNER = '{schemaName}' AND TABLE_NAME LIKE 'HF_%'";
             }
             else
             {
-                tableExistsQuery = @"SELECT TABLE_NAME
-FROM all_tables
-WHERE TABLE_NAME LIKE 'HF_%'
-ORDER BY TABLE_NAME";
+                tableExistsQuery = @"SELECT TABLE_NAME FROM all_tables WHERE TABLE_NAME LIKE 'HF_%'";
             }
 
             return connection.ExecuteScalar<string>(tableExistsQuery) != null;
+        }
+
+        private static bool IndexExists(IDbConnection connection, string schemaName)
+        {
+            string indexExistsQuery;
+
+            if (!string.IsNullOrEmpty(schemaName))
+            {
+                indexExistsQuery = $@"SELECT index_name FROM all_indexes WHERE OWNER = '{schemaName}' AND INDEX_NAME = 'HF_JOB_STATEID'";
+            }
+            else
+            {
+                indexExistsQuery = @"SELECT index_name FROM all_indexes WHERE INDEX_NAME = 'HF_JOB_STATEID'";
+            }
+
+            return connection.ExecuteScalar<string>(indexExistsQuery) != null;
         }
 
         private static string GetStringResource(string resourceName)
